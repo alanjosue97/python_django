196ff5a8c3970e2a6586a1030e8c5458983dcaf6
alanjosue97 <perezaln.09@gmail.com>
2023-07-04 00:25:26 -0600

    inventario CRUD

--- inventory/views.py
+++ inventory/views.py
@@ -21,17 +21,152 @@ def create(request):
             category = data["category"],
             identifier = data["identifier"],
         ) 
-        return HttpResponse("Created")
+        #return HttpResponse("Created")
+        return JsonResponse(
+        
+            data,
+            status=201,
+
+        )
 
     else:
-        return HttpResponse("Not Allowed")
+        #return HttpResponse("Not Allowed")
+        return JsonResponse(
+            {
+            "message": "Method not allow"
+            },
+            status=405,
+
+        )
     
 def list(request):
     if request.method == "GET":
         response = [
-            {"name": inventory.name,
+            {
+            "id": inventory.id,
+            "name": inventory.name,
              "price": inventory.price,
              "category": inventory.category,
              "identifier": inventory.identifier,
              } for inventory in Inventory.objects.all()]
         return JsonResponse({"data": response }, status=200)
+    else: 
+        return JsonResponse(
+            {
+                "messenge": "Not Allawed"
+            },
+            status=405,
+        )
+    
+def get(request, id: int):
+    if request.method =="GET":
+        try:
+            response = Inventory.objects.get(pk=id)
+        
+            return JsonResponse(
+            {
+            "id": response.id,
+            "name": response.name,
+             "price": response.price,
+             "category": response.category,
+             "identifier": response.identifier,
+            },
+            status=200)
+        except Inventory.DoesNotExist:
+            return JsonResponse(
+            {
+            "message": f"Objcet with {id} does not exists",
+            
+            }, status=404)
+
+    else:
+        return JsonResponse(
+            {
+                "message": "Method not allowed"
+            },
+            status=405,
+        )
+    
+def update(request, id: int):
+    data = json.loads(request.body)
+
+
+    if request.method == "PUT":
+        
+        try:
+            object_to_update = Inventory.objects.get(pk=id)
+
+            object_to_update.name = data["name"]
+            object_to_update.price = data["price"]
+            object_to_update.category = data["category"]
+            object_to_update.identifier = data["identifier"]
+
+            object_to_update.save()
+
+            Inventory.objects.get(pk=id)
+            return JsonResponse(
+                {
+                    "id": object_to_update.id,
+                    "name": object_to_update.name,
+                    "price": object_to_update.price,
+                    "category": object_to_update.category,
+                    "identifier": object_to_update.identifier,
+                },
+                status = 200,
+            )
+        except Inventory.DoesNotExist:
+            return JsonResponse(
+                {
+                    "message": f"Object with {id} does not exists",
+                },
+                status = 404,
+            )
+        except Inventory.DoesNotExist:
+            return JsonResponse(
+                {
+                "message": "Internal Server Error",
+                },
+                status=500,
+            )
+    else:
+        return JsonResponse(
+            {
+                "message": "Method not allowed"
+            },
+            status = 405,
+        )
+
+def delete(request, id: int):
+
+    if request.method == "DELETE":
+        
+        try:
+            object_to_update = Inventory.objects.get(pk=id)
+
+            
+            object_to_update.delete()
+
+            
+            return JsonResponse({},status = 204)
+            
+        except Inventory.DoesNotExist:
+            return JsonResponse(
+                {
+                    "message": f"Object with {id} does not exists",
+                },
+                status = 404,
+            )
+        except Inventory.DoesNotExist:
+            return JsonResponse(
+                {
+                "message": "Internal Server Error",
+                },
+                status=500,
+            )
+    else:
+        return JsonResponse(
+            {
+                "message": "Method not allowed"
+            },
+            status = 405,
+        )
